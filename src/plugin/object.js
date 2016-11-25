59cdd419d415f89cc46195c81fdb644d7c6dcc54
linyaosky <1245807623@qq.com>
2016-11-25 18:16:23 +0800

    js

---
+++ src/plugin/object.js
@@ -0,0 +1,20 @@
+//js对象：声明形式
+var Obj1 = {
+	name: 'ooj1'
+	// ...
+};
+
+//构造形式
+var Obj2 = new Object();
+Obj2.name = 'obj2';
+
+//js的基本类型。
+//string,number,boolean,null,undefined,object
+
+//内建对象。js在基本类型和对象之间做了很多的自动转换。
+String,Number,Boolean,Object,Function,Array,Date,RegExp,Error
+
+//访问对象的属性
+console.log(Obj1.name);
+console.log(Obj1["name"]);
+
